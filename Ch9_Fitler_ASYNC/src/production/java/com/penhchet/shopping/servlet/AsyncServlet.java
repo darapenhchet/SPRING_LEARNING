7797d8239352d7baa875b46b145abde9c81820e8
darapenhchet <darapenhchet@gmail.com>
2015-08-07 10:33:40 -0700

    Remove Super.doPost();

---
+++ Ch9_Fitler_ASYNC/src/production/java/com/penhchet/shopping/servlet/AsyncServlet.java
@@ -0,0 +1,71 @@
+package com.penhchet.shopping.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+			name = "asyncServlet",
+			urlPatterns ="/async",
+			asyncSupported = true
+		)
+public class AsyncServlet extends HttpServlet{
+	
+	private static volatile int ID = 1;
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		final int id;
+		synchronized (AsyncServlet.class) {
+			id = ID++;
+			
+		}
+		long timeout = request.getParameter("timeout") == null ?
+							10_000 : Long.parseLong(request.getParameter("timeout"));
+		System.out.println("Enter AsyncServlet.doGet(). Request ID = " + id + ", isAsyncStarted = " + request.isAsyncStarted());
+		
+		final AsyncContext context = request.getParameter("unwrap") !=null ? 
+											request.startAsync() : request.startAsync(request, response);
+		context.setTimeout(timeout);
+		
+		System.out.println("Starting Asynchronous Thread. Request ID = " + id + ".");
+		
+	
+	}
+	
+	private static class AsyncThread
+	{
+		private final int id;
+		private final AsyncContext context;
+		
+		public AsyncThread(int id, AsyncContext context){
+			this.id = id;
+			this.context = context;
+		}
+		
+		public void doWorkd(){
+			System.out.println("Asynchronous thread started. Request ID =" + this.id + ".");
+			
+			try{
+				Thread.sleep(5_000L);
+			}catch(Exception ex){
+				ex.printStackTrace();
+			}
+			
+			HttpServletRequest request = (HttpServletRequest)this.context.getRequest();
+			
+			System.out.println("Done sleeping. Request ID = " + this.id + 
+							   ", URL = " + request.getRequestURL() + ".");
+			
+			this.context.dispatch("/WEB-INF/jsp/view/async.jsp");
+	        System.out.println("Asynchronous thread completed. Request ID = " + this.id + "."); 
+		}
+	}
+}
---
+++ Ch9_Fitler_ASYNC/webapp/WEB-INF/jsp/view/nonAsync.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Welcome to Filter NON ASYNC</title>
+</head>
+<body>
+	<h1>Welcome TO FILTER NON ASYNC</h1>
+
+</body>
+</html>
