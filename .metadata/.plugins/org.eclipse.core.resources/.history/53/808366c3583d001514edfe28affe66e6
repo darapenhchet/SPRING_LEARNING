15211c139b7d4263f8b3fd613e195c23eb956d60
darapenhchet <darapenhchet@gmail.com>
2015-08-07 16:42:37 -0700

    AAA

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/53/808366c3583d001514edfe28affe66e6
@@ -0,0 +1,53 @@
+package com.penhchet.shopping.filter;
+
+import java.io.IOException;
+import java.time.Instant;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.util.StopWatch;
+
+public class CompressionFilter implements Filter{
+
+	@Override
+	public void destroy() {
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		Instant time = Instant.now();
+		StopWatch timer = new StopWatch();
+		try{
+			timer.start();
+			chain.doFilter(request, response);
+		}finally{
+			timer.stop();
+			HttpServletRequest in = (HttpServletRequest)request;
+			HttpServletResponse out = (HttpServletResponse)response;
+			String length = out.getHeader("Content-Length");
+			if(length == null || length.length() ==0){
+				length ="-";
+				System.out.println(in.getRemoteAddr() + " - - [" + time +" ]" +
+						" \"" + in.getMethod() + " " + in.getRequestURI() + " " +
+						in.getProtocol() + " \" " + out.getStatus() + " " + length + " " + timer);
+				
+				
+			}
+		}
+	}
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+		
+	}
+
+}
--- Ch9_Fitler_ASYNC/src/production/java/com/penhchet/shopping/filter/CompressionFilter.java
+++ Ch9_Fitler_ASYNC/src/production/java/com/penhchet/shopping/filter/CompressionFilter.java
@@ -1,6 +1,7 @@
 package com.penhchet.shopping.filter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.time.Instant;
 
 import javax.servlet.Filter;
@@ -11,6 +12,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
 
 import org.springframework.util.StopWatch;
 
@@ -24,30 +26,12 @@ public class CompressionFilter implements Filter{
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
-		Instant time = Instant.now();
-		StopWatch timer = new StopWatch();
-		try{
-			timer.start();
-			chain.doFilter(request, response);
-		}finally{
-			timer.stop();
-			HttpServletRequest in = (HttpServletRequest)request;
-			HttpServletResponse out = (HttpServletResponse)response;
-			String length = out.getHeader("Content-Length");
-			if(length == null || length.length() ==0){
-				length ="-";
-				System.out.println(in.getRemoteAddr() + " - - [" + time +" ]" +
-						" \"" + in.getMethod() + " " + in.getRequestURI() + " " +
-						in.getProtocol() + " \" " + out.getStatus() + " " + length + " " + timer);
-				
-				
-			}
+		if(((HttpServletRequest)request).getHeader("Accept-Encoding").contains("gzip")){
+			
 		}
 	}
 
 	@Override
 	public void init(FilterConfig config) throws ServletException {
-		
 	}
-
 }
