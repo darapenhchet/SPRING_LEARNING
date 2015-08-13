67807e5d7d5b81cbc722cc490fd6064a9a6ed195
darapenhchet <darapenhchet@gmail.com>
2015-08-13 08:42:44 +0700

    User

--- Ch13-Model-View-Controller/src/com/spring/config/ServletContextConfiguration.java
+++ Ch13-Model-View-Controller/src/com/spring/config/ServletContextConfiguration.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.RequestToViewNameTranslator;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.DefaultRequestToViewNameTranslator;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -26,6 +28,11 @@ public class ServletContextConfiguration {
 		resolver.setPrefix("/WEB-INF/jsp/view/");
 		resolver.setSuffix(".jsp");
 		return resolver;
+	}
+	
+	@Bean
+	public RequestToViewNameTranslator viewNameTranslator(){
+		return new DefaultRequestToViewNameTranslator();
 	}
 
 }
