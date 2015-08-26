d3e2c28cadeb981aaeb3c125faf8db39ebe5165f
darapenhchet <darapenhchet@gmail.com>
2015-08-26 16:07:52 +0700

    - Collection Mapping

--- hibernate-course/src/main/java/com/penhchet/data/entities/User.java
+++ hibernate-course/src/main/java/com/penhchet/data/entities/User.java
@@ -2,45 +2,97 @@ package com.penhchet.data.entities;
 
 import java.util.Date;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.Formula;
 
 @Entity
-@Table(name="FINANCES_USER")
+@Table(name = "FINANCES_USER")
 public class User {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="USER_ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//SEQUENCE GENERATION
+	//@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="user_seq")
+	//@SequenceGenerator(name = "user_seq", sequenceName="USER_ID_SEQ")
+	//TABLE GENERATION
+	//@GeneratedValue(strategy = GenerationType.TABLE, generator="user_table_generator")
+	//@TableGenerator(name="user_table_generator",
+	//	table ="IFINANCES_KEYS", pkColumnName="PK_NAME", valueColumnName= "PK_VALUE")
+	@Column(name = "USER_ID")
 	private Long userId;
 
-	@Column(name="FIRST_NAME")
+	@Column(name = "FIRST_NAME")
 	private String firstName;
 
-	@Column(name="LAST_NAME")
+	@Column(name = "LAST_NAME")
 	private String lastName;
 
-	@Column(name="BIRTH_DATE")
+	@Column(name = "BIRTH_DATE", nullable=false)
 	private Date birthDate;
 
-	@Column(name="EMAIL_ADDRESS")
+	@Column(name = "EMAIL_ADDRESS")
 	private String emailAddress;
 
-	@Column(name="LAST_UPDATED_DATE")
+	@Column(name = "LAST_UPDATED_DATE")
 	private Date lastUpdatedDate;
 
-	@Column(name="LAST_UPDATED_BY")
+	@Column(name = "LAST_UPDATED_BY")
 	private String lastUpdatedBy;
 
-	@Column(name="CREATED_DATE")
+	@Column(name = "CREATED_DATE", updatable=false)
 	private Date createdDate;
 
-	@Column(name="CREATED_BY")
+	@Column(name = "CREATED_BY", updatable=false)
 	private String createdBy;
+	
+	@Formula("lower(datediff(curdate(), birth_date)/365)")
+	private int age;
+	
+	@Embedded
+	@AttributeOverrides({
+		@AttributeOverride(name="addressLine1", column=@Column(name="USER_ADDRESS_LINE_1")),
+		@AttributeOverride(name="addressLine2", column=@Column(name="USER_ADDRESS_LINE_2"))
+	})
+	private Address address;
+	
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Transient
+	private boolean valid;
+
+	public boolean isValid() {
+		return valid;
+	}
+
+	public void setValid(boolean valid) {
+		this.valid = valid;
+	}
 
 	public Long getUserId() {
 		return userId;
