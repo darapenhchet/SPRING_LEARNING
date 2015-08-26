0a1a5a1ff079ed7961f38743a523a53d8881ab13
darapenhchet <darapenhchet@gmail.com>
2015-08-26 12:03:31 +0700

    Hibernate-Course

---
+++ Hibernate/source/production/java/com/spring/site/controller/web/AccountController.java
@@ -0,0 +1,12 @@
+package com.spring.site.controller.web;
+
+import javax.inject.Inject;
+
+import com.spring.config.annotation.WebController;
+import com.spring.site.service.AccountService;
+
+@WebController
+public class AccountController {
+
+	//@Inject AccountService accountService;
+}
